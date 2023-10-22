35d7ea7289ba927ab417451e2b40cd08a6460eac
jackson18 <qijiabin18@126.com>
2023-10-22 22:20:21 +0800

    动态生成类‘

--- bytebuddy-test/src/test/java/cn/javabeanqi/bytebuddy/ByteBuddyTest.java
+++ bytebuddy-test/src/test/java/cn/javabeanqi/bytebuddy/ByteBuddyTest.java
@@ -1,6 +1,13 @@
 package cn.javabeanqi.bytebuddy;
 
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.NamingStrategy;
+import net.bytebuddy.dynamic.DynamicType;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * ========================================================
@@ -13,12 +20,29 @@ import org.junit.Test;
  */
 public class ByteBuddyTest {
 
+    private String path;
+
+    @Before
+    public void init() {
+        path = ByteBuddyTest.class.getClassLoader().getResource("").getPath();
+        System.out.println("path = " + path);
+    }
+
     /**
      * 生成一个类
+     * Object$ByteBuddy$VQy9xVU2
      */
     @Test
-    public void createClassTest() {
-
+    public void createClassTest() throws IOException {
+        // unloaded 代表生成的字节码
+        DynamicType.Unloaded<UserService> unloaded = new ByteBuddy()
+                // 指定类的生成策略
+                .with(new NamingStrategy.SuffixingRandom("javabeanqi"))
+                .subclass(UserService.class)
+                .name("cn.javabeanqi.MyUserService")
+                .make();
+        // 保存获取生成类的字节码
+        unloaded.saveIn(new File(path));
     }
 
 }
