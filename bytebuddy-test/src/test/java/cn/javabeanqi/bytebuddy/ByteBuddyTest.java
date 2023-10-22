b266e0180d4e34a348c43d8b61ad2ddacf8e172e
jackson18 <qijiabin18@126.com>
2023-10-22 21:51:53 +0800

    初始化工程

---
+++ bytebuddy-test/src/test/java/cn/javabeanqi/bytebuddy/ByteBuddyTest.java
@@ -0,0 +1,24 @@
+package cn.javabeanqi.bytebuddy;
+
+import org.junit.Test;
+
+/**
+ * ========================================================
+ * 日 期：2023/10/22 21:40
+ * 作 者：jiabinqi
+ * 版 本：1.0.0
+ * 类说明：
+ * ========================================================
+ * 修订日期     修订人    描述
+ */
+public class ByteBuddyTest {
+
+    /**
+     * 生成一个类
+     */
+    @Test
+    public void createClassTest() {
+
+    }
+
+}
